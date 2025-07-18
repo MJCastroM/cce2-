5cd8801c6cd454b6f3ada7c3d13c090a64c91f13
AndresFeijo <163592544+AndresFeijo@users.noreply.github.com>
2025-07-18 13:16:37 -0300

    Instrucciones de uso
    Agrego instrucciones de uso y posibilidad de indicar el nombre de salida del archivo

--- decoder.cpp
+++ decoder.cpp
@@ -195,9 +195,12 @@ vector<uint8_t> decodificador(vector<uint8_t> bloque_con_ruido, int N, int K) {
         poly a((N-K), 0); a.push_back(1); // a(x) = x^{d-1} r = n − k = d − 1)
         euclid(a, sindromes, N - K, pol_loc_err, pol_ev_err);
         vector<int> error_positions = chien_search(pol_loc_err, N);
-        if (pol_loc_err.size() - 1 > error_positions.size())
+        if (pol_loc_err.size() - 1 > error_positions.size()) {
             cout << "Hay " << pol_loc_err.size() - 1 << " errores y se encontraron "<< error_positions.size()<< " raices" << endl;
-        forney_correct(bloque_con_ruido, pol_loc_err, pol_ev_err, error_positions); 
+            print_poly(pol_loc_err, "pol_loc_err");
+            print_poly(pol_ev_err, "pol_ev_err");
+        }
+            forney_correct(bloque_con_ruido, pol_loc_err, pol_ev_err, error_positions); 
          for (int i=0; i<N; i++) { 
             if(bloque_con_ruido[i] != bloque_original[i]) {
                 cout << i 
--- rsdecode.cpp
+++ rsdecode.cpp
@@ -26,7 +26,9 @@ void guardar(const vector<uint8_t>& datos, const string& nombre_archivo) {
 bool leerYProcesarBloques(const string& nombreArchivo, int tamanio_bloque, int redundancia, string nombreDestino) {
     ifstream archivo(nombreArchivo, ios::binary);
     if (!archivo.is_open()) {
-        cerr << "No se pudo abrir el archivo binario: " << nombreArchivo << endl;
+        cerr << "No se pudo abrir el archivo: " << nombreArchivo << endl;
+        cout << "Recuerde:" << endl;
+        cout << "Uso: rsdecode.exe [m] [r] archivoentrada.ext [archivosalida]" << endl;
         return false;
     }
 
@@ -49,9 +51,28 @@ bool leerYProcesarBloques(const string& nombreArchivo, int tamanio_bloque, int r
 }
 
 int main(int argc, char* argv[]) {
-    string nombreDestino = argv[3];
-    size_t punto = nombreDestino.rfind('.');
-    nombreDestino = nombreDestino.substr(0, punto) + ".out";
+    if (argc < 4 || argc > 5) {
+        cout << "Uso: rsdecode.exe [m] [r] archivoentrada.ext [archivosalida]" << endl;
+        cout << endl;
+        cout << "archivoentrada             : archivo a decodificar" << endl;
+        cout << "archivosalida              : archivo decodificado (por defecto: 'archivoentrada.out')" << endl;
+        cout << endl;
+        cout << "Parametros:" << endl;
+        cout << "m <largocodigo>            : largo del codigo" << endl;
+        cout << "r <redundancia>            : redundancia del codigo" << endl;
+        cout << endl;
+        cout << "Nota: debe especificar los parametros m, r y la entrada." << endl;
+        return 0;
+    }
+    string nombreDestino;
+    if (argc == 5) {
+        nombreDestino = argv[4];
+    } else {
+        nombreDestino = argv[3];
+        size_t punto = nombreDestino.rfind('.');
+        nombreDestino = nombreDestino.substr(0, punto) + ".out";
+    }
+    
 
     // Crear archivo vacío al inicio (truncar si ya existe)
     ofstream limpiar(nombreDestino, ios::trunc | ios::binary);
